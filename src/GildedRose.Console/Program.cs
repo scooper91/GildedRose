10e39e5837f096b7743280623b480b124e99f57b
Chris Watson <chris24watson@gmail.com>
2014-10-22 14:49:47 +0100

    Refactored and removed dead code

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -8,32 +8,26 @@ namespace GildedRose.Console
 
 		private static void Main()
 		{
-			System.Console.WriteLine("OMGHAI!");
-
-			var app = new Program
-			{
-				Items = new List<Item>
-				{
-					new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-					new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-					new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-					new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-					new Item
-					{
-						Name = "Backstage passes to a TAFKAL80ETC concert",
-						SellIn = 15,
-						Quality = 20
-					},
-					new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
-				}
-
-			};
-
-			app.UpdateQuality();
-			System.Console.ReadKey();
+            new Program
+            {
+                Items = new List<Item>
+		        {
+		            new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+		            new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+		            new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+		            new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+		            new Item
+		            {
+		                Name = "Backstage passes to a TAFKAL80ETC concert",
+		                SellIn = 15,
+		                Quality = 20
+		            },
+		            new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+		        }
+            };
 		}
 
-		public void UpdateQuality()
+	    public void UpdateQuality()
 		{
 			foreach (var item in Items)
 			{
@@ -52,14 +46,14 @@ namespace GildedRose.Console
 					{
 						changeInQuality++;
 
-						if (IsBackstagePass(item))
+                        if (IsBackstagePass(item) && item.Quality < 50)
 						{
-							if (item.SellIn < 11 && item.Quality < 50)
+							if (item.SellIn < 11)
 							{
 								changeInQuality++;
 							}
 
-							if (item.SellIn < 6 && item.Quality < 50)
+							if (item.SellIn < 6)
 							{
 								changeInQuality++;
 							}
@@ -76,12 +70,9 @@ namespace GildedRose.Console
 				{
 					if (IsNotAgedBrie(item))
 					{
-						if (!IsBackstagePass(item))
+						if (!IsBackstagePass(item) && item.Quality > 0)
 						{
-							if (item.Quality > 0)
-							{
-								changeInQuality -= CalculateDecrease(item);
-							}
+							changeInQuality -= CalculateDecrease(item);
 						}
 						else
 						{
